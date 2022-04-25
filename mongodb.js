9adffdca55ea130c3c32e9c66f7444898608a4f0
Recondito <santanallanes@hotmail.com>
2022-04-25 10:42:01 -0400

    Implemented InsertOne and InsertMany

--- mongodb.js
+++ mongodb.js
@@ -10,8 +10,46 @@ MongoClient.connect(connectionURL, { useNewUrlParser: true }, (error, client) =>
     }
     const db = client.db(databaseName)
     
-    db.collection('users').insertOne({
-        name: 'Li-leh',
-        age: 22
+    // db.collection('users').insertOne({
+    //     name: 'Li-leh',
+    //     age: 22
+    // }, (error, result) => {
+    //     if(error) {
+    //         return console.log('Unable to insert user.')
+    //     }
+    //     console.log(result.insertedId)
+    // })
+
+    // db.collection('users').insertMany([
+    //     {
+    //         name: 'Lanaerys',
+    //         age: 19
+    //     }, {
+    //         name: 'Alby',
+    //         age: 32
+    //     }
+    // ], (error, result) => {
+    //     if (error) {
+    //         return console.log('Unable to insert documents.')
+    //     }
+    //     console.log(result.insertedIds)
+    // })
+
+    db.collection('tasks').insertMany([
+        {
+            description: 'Learn MongoDB.',
+            completed: true
+        }, {
+            description: 'Get Byleth into Elite Smash.',
+            completed: false
+        }, {
+            description: 'Have Lunch.',
+            completed: true
+        }
+    ], (error, result) => {
+        if (error) {
+            return console.log('Unable to insert documents.')
+        }
+        console.log(result.insertedIds)
     })
 })
