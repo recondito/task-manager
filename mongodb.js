cad9ff4f570c851df39dec47ac66241ec8c5f09e
Recondito <santanallanes@hotmail.com>
2022-04-25 20:51:22 -0400

    Deleted commented out queries

--- mongodb.js
+++ mongodb.js
@@ -14,47 +14,4 @@ MongoClient.connect(connectionURL, { useNewUrlParser: true }, (error, client) =>
         return console.log('Unable to connect to database.')
     }
     const db = client.db(databaseName)
-    
-    // db.collection('users').insertOne({
-    //     name: 'Astaroth',
-    //     age: 66
-    // }, (error, result) => {
-    //     if(error) {
-    //         return console.log('Unable to insert user.')
-    //     }
-    //     console.log(result.insertedId)
-    // })
-
-    // db.collection('users').insertMany([
-    //     {
-    //         name: 'Lanaerys',
-    //         age: 19
-    //     }, {
-    //         name: 'Alby',
-    //         age: 32
-    //     }
-    // ], (error, result) => {
-    //     if (error) {
-    //         return console.log('Unable to insert documents.')
-    //     }
-    //     console.log(result.insertedIds)
-    // })
-
-    // db.collection('tasks').insertMany([
-    //     {
-    //         description: 'Learn MongoDB.',
-    //         completed: true
-    //     }, {
-    //         description: 'Get Byleth into Elite Smash.',
-    //         completed: false
-    //     }, {
-    //         description: 'Have Lunch.',
-    //         completed: true
-    //     }
-    // ], (error, result) => {
-    //     if (error) {
-    //         return console.log('Unable to insert documents.')
-    //     }
-    //     console.log(result.insertedIds)
-    // })
 })
