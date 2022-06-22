26814139f6b069038ac1539b75a67cd84ace9396
Recondito <santanallanes@hotmail.com>
2022-06-22 19:31:36 -0400

    =Removed unused declarations. Added bcryptjs.

--- src/index.js
+++ src/index.js
@@ -1,7 +1,5 @@
 const express = require('express')
 require('./db/mongoose')
-const User = require('./models/user')
-const Task = require('./models/task')
 const userRouter = require('./routers/user')
 const taskRouter = require('./routers/task')
 
