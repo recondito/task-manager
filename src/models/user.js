5112598dbb0d1155480743ba009efff196575386
Recondito <santanallanes@hotmail.com>
2022-07-30 20:06:36 -0400

    Safety issues corrected.

--- src/models/user.js
+++ src/models/user.js
@@ -65,7 +65,7 @@ userSchema.virtual('tasks', {
 
 userSchema.methods.generateAuthToken = async function () {
     const user = this
-    const token = jwt.sign({ _id: user.id.toString() }, 'somerandomsentence')
+    const token = jwt.sign({ _id: user.id.toString() }, process.env.JWT_SECRET)
 
     user.tokens = user.tokens.concat({ token })
     await user.save()
